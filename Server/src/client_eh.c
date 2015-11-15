0fb898ad372b1d151f5fbe163a5a3319e7486e9d
TFK1410 <tfk1410@gmail.com>
2015-11-15 20:13:59 +0100

    Added simple return of ipv4 address and mask

--- Server/src/client_eh.c
+++ Server/src/client_eh.c
@@ -1,17 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
 #include "client_eh.h"
 
 struct eh_ctx {
     int fd;
 };
 
-int sendbytes(const char* msg, int fd);
-size_t receivebytes(int fd, char **msg);
-int disc_user(int epoll_fd, struct epoll_event *es, struct epoll_event *e);
+static int sendbytes(const char* msg, int fd);
+static size_t receivebytes(int fd, char **msg);
+//static int disc_user(int epoll_fd, struct epoll_event *es, struct epoll_event *e);
 
-size_t receivebytes(int fd, char **msg){
+static size_t receivebytes(int fd, char **msg){
     char buff[10000];
     size_t len=0;
 
@@ -25,13 +27,35 @@ size_t receivebytes(int fd, char **msg){
     return len;
 }
 
-int sendbytes(const char* msg, int fd){
+static int sendbytes(const char* msg, int fd){
     size_t len=strlen(msg);
 
     if(len>0)
         write(fd,msg,len);
 
     return 0;
+}
+
+static void handle_ifconfig(char* iface, int fd){
+    struct ifreq ifr = {};
+    char message[1000] = "\0";
+    iface[strlen(iface)-2] = '\0';
+
+    ifr.ifr_addr.sa_family = AF_INET;
+
+    if(strlen(iface)<IFNAMSIZ-1)
+        strncpy(ifr.ifr_name , iface , strlen(iface));//iface - interface name
+    else
+        strncpy(ifr.ifr_name , iface , IFNAMSIZ-1);//iface - interface name
+
+    ioctl(fd, SIOCGIFADDR, &ifr);//ipv4
+    strcat(message, inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr));
+
+    ioctl(fd, SIOCGIFNETMASK, &ifr);//net_mask
+    strcat(message, inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr));
+    strcat(message, "\n");
+
+    sendbytes(message,fd);
 }
 
 /*
@@ -57,7 +81,8 @@ static int handle_event(event_handler *self, const struct epoll_event *e){
     else if (e->events & EPOLLIN) {
         msg_len = receivebytes(e->data.fd, &buff);
         if (msg_len > 1) {
-            sendbytes("1.1.Unknown message\n",e->data.fd);
+            handle_ifconfig(buff,e->data.fd);
+            //sendbytes("1.1.Unknown message\n",e->data.fd);
         } else
             return 1;
         if (buff){ free(buff); buff = NULL; }
