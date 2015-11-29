02a593398f504fd4cfeb1ae3b7a35311989bd953
michalbednarski <bednarski.michal2@gmail.com>
2015-11-29 14:08:24 +0100

    Add test for handle_set_mac in client_eh

--- Server/test/client_eh_test.cpp
+++ Server/test/client_eh_test.cpp
@@ -7,6 +7,7 @@ extern "C" {
 
 #include "MockSyscalls.h"
 #include "MockIfconfigurator.h"
+#include <net/if_arp.h>
 
 using namespace ::testing;
 
@@ -113,6 +114,37 @@ TEST(client_eh_test, test_set_ip)
         EXPECT_CALL(mockIfconfigurator, set_ip(StrEq("if0"), Field(&in_addr::s_addr, htonl(0x01020304))))
             .WillOnce(Return(true));
         EXPECT_CALL(mockIfconfigurator, set_net_mask(StrEq("if0"), Field(&in_addr::s_addr, htonl(0xf8000000))))
+            .WillOnce(Return(true));
+        EXPECT_FUNCTION_CALL(writeMock, (9, _, _))
+            .WillOnce(MockedWrite(outcomingMessage, outcomingMessageLen));
+    }
+
+    // Test logic
+    struct event_handler *eh = create_client_eh(9, mockIfconfigurator.getStruct());
+    eh->handle_event(eh, EPOLLIN);
+    EXPECT_FUNCTION_CALL(closeMock, (9)).WillOnce(Return(0));
+    eh->destroy(eh);
+}
+
+TEST(client_eh_test, test_set_mac)
+{
+    // Input text
+    const char *incomingMessage = "setmac if0 12:34:56:AB:cd:EF";
+    int incomingMessageLen = strlen(incomingMessage);
+
+    // Expected output
+    const char *outcomingMessage = "Successfully changed mac address of the interface if0 to 12:34:56:AB:cd:EF\n";
+    int outcomingMessageLen = strlen(outcomingMessage);
+
+    // Mocks
+    MockIfconfigurator mockIfconfigurator;
+    ReadSyscallMock readMock;
+    WriteSyscallMock writeMock;
+    CloseSyscallMock closeMock;
+    {
+        EXPECT_FUNCTION_CALL(readMock, (9, _, Gt(incomingMessageLen)))
+            .WillOnce(MockedRead(incomingMessage, incomingMessageLen));
+        EXPECT_CALL(mockIfconfigurator, set_mac(StrEq("if0"), Field(&sockaddr::sa_family, ARPHRD_ETHER)))
             .WillOnce(Return(true));
         EXPECT_FUNCTION_CALL(writeMock, (9, _, _))
             .WillOnce(MockedWrite(outcomingMessage, outcomingMessageLen));
