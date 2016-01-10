7aec62fc5b427bf1b76caee040ebacfd36038cfe
michalbednarski <bednarski.michal2@gmail.com>
2016-01-10 11:25:28 +0100

    Update ifconfigurator tests for new implementation

--- Server/test/ifconfigurator_test.cpp
+++ Server/test/ifconfigurator_test.cpp
@@ -6,98 +6,106 @@ extern "C" {
 
 #include <sys/ioctl.h>
 #include <net/if.h>
+#include <linux/netlink.h>
 
 #include <gtest/gtest.h>
 
 using namespace ::testing;
 
-static FILE *fixtureFopen(const char *, const char *) {
-    return FopenCallMock::real("../test/if_inet6_fixture.txt", "r");
+static const struct ifconfig *getIfconfigFromMessage(const void *buf)
+{
+    return (struct ifconfig *)((char *)buf + NLMSG_HDRLEN);
 }
 
-TEST(ifconfigurator_test, test_get_if_config)
+class ifconfigurator_test : public ::testing::Test
 {
-    SocketSyscallMock socketSyscallMock;
-    IoctlSyscallMock ioctlSyscallMock;
-    FopenCallMock fopenCallMock;
-    CloseSyscallMock closeSyscallMock;
+protected:
+    static const int netlink_fd = 6;
+    ifconfigurator *ifc;
+    virtual void SetUp()
+    {
+        SocketSyscallMock socketSyscallMock;
+        BindSyscallMock bindSyscallMock;
 
-    // Create ifconfigurator
-    EXPECT_FUNCTION_CALL(socketSyscallMock, (AF_INET, _, _))
-        .WillOnce(Return(6));
-    ifconfigurator *ifc = create_ifconfigurator();
+        // Create ifconfigurator
+        EXPECT_FUNCTION_CALL(socketSyscallMock, (AF_NETLINK, _, _))
+            .WillOnce(Return(netlink_fd));
+        EXPECT_FUNCTION_CALL(bindSyscallMock, (netlink_fd, _, _))
+            .WillOnce(Return(0));
+        ifc = create_ifconfigurator();
+     }
 
-    // Request we expect
-    struct ifreq expectedRequest = {};
-    expectedRequest.ifr_addr.sa_family = AF_INET;
-    strcpy(expectedRequest.ifr_name, "if0");
+    virtual void TearDown()
+    {
+        CloseSyscallMock closeSyscallMock;
 
-    // Return values
-    struct ifreq flagsResult = expectedRequest;
-    flagsResult.ifr_flags = IFF_RUNNING;
+        // Destory ifconfigurator
+        EXPECT_FUNCTION_CALL(closeSyscallMock, (netlink_fd))
+            .WillOnce(Return(0));
+        ifc->destroy(ifc);
+    }
+};
 
-    struct ifreq hwAddrResult = expectedRequest;
-    struct ifreq addrResult = expectedRequest;
-    struct ifreq brdAddrResult = expectedRequest;
-    struct ifreq netmaskResult = expectedRequest;
+MATCHER_P(SockAddrInMatches, expectedAddr, "")
+{
+    return arg.sin_family == AF_INET && memcmp(&arg.sin_addr, expectedAddr, sizeof(sockaddr_in::sin_addr)) == 0;
+}
+
+MATCHER_P(SockAddrMatches, expectedAddr, "")
+{
+    return memcmp(&arg, expectedAddr, sizeof(sockaddr_in)) == 0;
+}
+
+TEST_F(ifconfigurator_test, test_get_if_config)
+{
+    SendToSyscallMock sendToSyscallMock;
+    RecvSyscallMock recvSyscallMock;
+
+    // Prepare response
+    char responseBuffer[NLMSG_SPACE(sizeof(struct ifconfig))];
+    memset(responseBuffer, 0, sizeof(responseBuffer));
+
+    // Put pattern with marked start/end in response buffer
+    // to ensure that it won't be modified after being returned
+    // from kernel and copied correctly
+    char *responseData = (char *)NLMSG_DATA(responseBuffer);
+    memset(responseData, 0xBB, sizeof(struct ifconfig));
+    responseData[0] = 0xAB;
+    responseData[sizeof(struct ifconfig) - 1] = 0xBC;
+
 
     // Request interface information
-    EXPECT_FUNCTION_CALL(ioctlSyscallMock, (6, SIOCGIFFLAGS, _))
-        .WillOnce(DoAll(
-                MemCmpArg<2>(&expectedRequest, sizeof(struct ifreq)),
-                MemCpyArg<2>(&flagsResult, sizeof(struct ifreq)),
-                Return(0)
-        ));
-    EXPECT_FUNCTION_CALL(ioctlSyscallMock, (6, SIOCGIFHWADDR, _))
-        .WillOnce(DoAll(
-                MemCmpArg<2>(&expectedRequest, sizeof(struct ifreq)),
-                MemCpyArg<2>(&hwAddrResult, sizeof(struct ifreq)),
-                Return(0)
-        ));
-    EXPECT_FUNCTION_CALL(ioctlSyscallMock, (6, SIOCGIFADDR, _))
-        .WillOnce(DoAll(
-                MemCmpArg<2>(&expectedRequest, sizeof(struct ifreq)),
-                MemCpyArg<2>(&addrResult, sizeof(struct ifreq)),
-                Return(0)
-        ));
-    EXPECT_FUNCTION_CALL(ioctlSyscallMock, (6, SIOCGIFBRDADDR, _))
-        .WillOnce(DoAll(
-                MemCmpArg<2>(&expectedRequest, sizeof(struct ifreq)),
-                MemCpyArg<2>(&brdAddrResult, sizeof(struct ifreq)),
-                Return(0)
-        ));
-    EXPECT_FUNCTION_CALL(ioctlSyscallMock, (6, SIOCGIFNETMASK, _))
-        .WillOnce(DoAll(
-                MemCmpArg<2>(&expectedRequest, sizeof(struct ifreq)),
-                MemCpyArg<2>(&netmaskResult, sizeof(struct ifreq)),
-                Return(0)
-        ));
-    EXPECT_FUNCTION_CALL(fopenCallMock, (StrEq("/proc/net/if_inet6"), StrEq("r")))
-        .WillOnce(Invoke(fixtureFopen));
+    EXPECT_FUNCTION_CALL(sendToSyscallMock,
+                (
+                    6, // sockfd
+                    ResultOf(getIfconfigFromMessage, Field(&ifconfig::message_type, Eq(LACPM_SHOW))), // buf
+                    sizeof(responseBuffer), // len
+                    _, _, _ // flags, dest_addr, addrlen
+                )
+            )
+            .WillOnce(ReturnArg<2>());
+    EXPECT_FUNCTION_CALL(recvSyscallMock, (6, _, sizeof(responseBuffer), _))
+            .WillOnce(DoAll(
+                    MemCpyArg<1>(responseBuffer, sizeof(responseBuffer)),
+                    ReturnArg<2>()
+            ));
 
     struct ifconfig out_config;
     memset(&out_config, 0xCC, sizeof(out_config));
     ifc->get_if_config(ifc, "if0", &out_config);
 
-    EXPECT_EQ(IFF_RUNNING, out_config.flags);
-    EXPECT_EQ(0, memcmp(out_config.ipv6, "\xfe\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x30\x5f", 16));
-
-    // Destory ifconfigurator
-    EXPECT_FUNCTION_CALL(closeSyscallMock, (6))
-        .WillOnce(Return(0));
-    ifc->destroy(ifc);
+    // Assert that data from kernel were returned unaltered
+    EXPECT_EQ(0, memcmp(&out_config, responseData, sizeof(struct ifconfig)));
 }
 
-TEST(ifconfigurator_test, test_set_ip)
+TEST_F(ifconfigurator_test, test_set_ip)
 {
-    SocketSyscallMock socketSyscallMock;
-    IoctlSyscallMock ioctlSyscallMock;
-    CloseSyscallMock closeSyscallMock;
+    SendToSyscallMock sendToSyscallMock;
+    RecvSyscallMock recvSyscallMock;
 
-    // Create ifconfigurator
-    EXPECT_FUNCTION_CALL(socketSyscallMock, (AF_INET, _, _))
-        .WillOnce(Return(6));
-    ifconfigurator *ifc = create_ifconfigurator();
+    // Prepare response
+    char responseBuffer[NLMSG_SPACE(sizeof(struct ifconfig))];
+    memset(responseBuffer, 0, sizeof(responseBuffer));
 
     // Request we expect
     struct in_addr newAddr = {};
@@ -109,29 +117,36 @@ TEST(ifconfigurator_test, test_set_ip)
     memcpy(&((struct sockaddr_in *) &expectedRequest.ifr_addr)->sin_addr, &newAddr, sizeof(newAddr));
 
     // Request interface information
-    EXPECT_FUNCTION_CALL(ioctlSyscallMock, (6, SIOCSIFADDR, _))
-        .WillOnce(DoAll(
-                MemCmpArg<2>(&expectedRequest, sizeof(struct ifreq)),
-                Return(0)
-        ));
+    EXPECT_FUNCTION_CALL(sendToSyscallMock,
+                (
+                    6, // sockfd
+                    // buf
+                    ResultOf(getIfconfigFromMessage, AllOf(
+                        Field(&ifconfig::message_type, Eq(LACPM_SETIP)),
+                        Field(&ifconfig::name, StrEq("if0")),
+                        Field(&ifconfig::ipv4, SockAddrInMatches(&newAddr))
+                    )),
+                    sizeof(responseBuffer), // len
+                    _, _, _ // flags, dest_addr, addrlen
+                )
+            )
+            .WillOnce(ReturnArg<2>());
+    EXPECT_FUNCTION_CALL(recvSyscallMock, (6, _, sizeof(responseBuffer), _))
+            .WillOnce(DoAll(
+                    MemCpyArg<1>(responseBuffer, sizeof(responseBuffer)),
+                    ReturnArg<2>()
+            ));
     EXPECT_EQ(true, ifc->set_ip(ifc, "if0", &newAddr));
-
-    // Destory ifconfigurator
-    EXPECT_FUNCTION_CALL(closeSyscallMock, (6))
-        .WillOnce(Return(0));
-    ifc->destroy(ifc);
 }
 
-TEST(ifconfigurator_test, test_set_netmask)
+TEST_F(ifconfigurator_test, test_set_netmask)
 {
-    SocketSyscallMock socketSyscallMock;
-    IoctlSyscallMock ioctlSyscallMock;
-    CloseSyscallMock closeSyscallMock;
+    SendToSyscallMock sendToSyscallMock;
+    RecvSyscallMock recvSyscallMock;
 
-    // Create ifconfigurator
-    EXPECT_FUNCTION_CALL(socketSyscallMock, (AF_INET, _, _))
-        .WillOnce(Return(6));
-    ifconfigurator *ifc = create_ifconfigurator();
+    // Prepare response
+    char responseBuffer[NLMSG_SPACE(sizeof(struct ifconfig))];
+    memset(responseBuffer, 0, sizeof(responseBuffer));
 
     // Request we expect
     struct in_addr newAddr = {};
@@ -140,53 +155,69 @@ TEST(ifconfigurator_test, test_set_netmask)
     struct ifreq expectedRequest = {};
     expectedRequest.ifr_addr.sa_family = AF_INET;
     strcpy(expectedRequest.ifr_name, "if0");
-    memcpy(&((struct sockaddr_in *) &expectedRequest.ifr_netmask)->sin_addr, &newAddr, sizeof(newAddr));
+    memcpy(&((struct sockaddr_in *) &expectedRequest.ifr_addr)->sin_addr, &newAddr, sizeof(newAddr));
 
     // Request interface information
-    EXPECT_FUNCTION_CALL(ioctlSyscallMock, (6, SIOCSIFNETMASK, _))
-        .WillOnce(DoAll(
-                MemCmpArg<2>(&expectedRequest, sizeof(struct ifreq)),
-                Return(0)
-        ));
+    EXPECT_FUNCTION_CALL(sendToSyscallMock,
+                (
+                    6, // sockfd
+                    // buf
+                    ResultOf(getIfconfigFromMessage, AllOf(
+                        Field(&ifconfig::message_type, Eq(LACPM_SETMASK)),
+                        Field(&ifconfig::name, StrEq("if0")),
+                        Field(&ifconfig::ipv4_netmask, SockAddrInMatches(&newAddr))
+                    )),
+                    sizeof(responseBuffer), // len
+                    _, _, _ // flags, dest_addr, addrlen
+                )
+            )
+            .WillOnce(ReturnArg<2>());
+    EXPECT_FUNCTION_CALL(recvSyscallMock, (6, _, sizeof(responseBuffer), _))
+            .WillOnce(DoAll(
+                    MemCpyArg<1>(responseBuffer, sizeof(responseBuffer)),
+                    ReturnArg<2>()
+            ));
     EXPECT_EQ(true, ifc->set_net_mask(ifc, "if0", &newAddr));
-
-    // Destory ifconfigurator
-    EXPECT_FUNCTION_CALL(closeSyscallMock, (6))
-        .WillOnce(Return(0));
-    ifc->destroy(ifc);
 }
 
-TEST(ifconfigurator_test, test_set_mac)
+TEST_F(ifconfigurator_test, test_set_mac)
 {
-    SocketSyscallMock socketSyscallMock;
-    IoctlSyscallMock ioctlSyscallMock;
-    CloseSyscallMock closeSyscallMock;
+    SendToSyscallMock sendToSyscallMock;
+    RecvSyscallMock recvSyscallMock;
 
-    // Create ifconfigurator
-    EXPECT_FUNCTION_CALL(socketSyscallMock, (AF_INET, _, _))
-        .WillOnce(Return(6));
-    ifconfigurator *ifc = create_ifconfigurator();
+    // Prepare response
+    char responseBuffer[NLMSG_SPACE(sizeof(struct ifconfig))];
+    memset(responseBuffer, 0, sizeof(responseBuffer));
 
     // Request we expect
     struct sockaddr newAddr = {};
     memset(&newAddr, 0xA1, sizeof(newAddr));
+    newAddr.sa_family = AF_INET;
 
     struct ifreq expectedRequest = {};
     expectedRequest.ifr_addr.sa_family = AF_INET;
     strcpy(expectedRequest.ifr_name, "if0");
-    memcpy(&expectedRequest.ifr_hwaddr, &newAddr, sizeof(newAddr));
+    memcpy(&((struct sockaddr_in *) &expectedRequest.ifr_addr)->sin_addr, &newAddr, sizeof(newAddr));
 
     // Request interface information
-    EXPECT_FUNCTION_CALL(ioctlSyscallMock, (6, SIOCSIFHWADDR, _))
-        .WillOnce(DoAll(
-                MemCmpArg<2>(&expectedRequest, sizeof(struct ifreq)),
-                Return(0)
-        ));
+    EXPECT_FUNCTION_CALL(sendToSyscallMock,
+                (
+                    6, // sockfd
+                    // buf
+                    ResultOf(getIfconfigFromMessage, AllOf(
+                        Field(&ifconfig::message_type, Eq(LACPM_SETMAC)),
+                        Field(&ifconfig::name, StrEq("if0")),
+                        Field(&ifconfig::mac, SockAddrMatches(&newAddr))
+                    )),
+                    sizeof(responseBuffer), // len
+                    _, _, _ // flags, dest_addr, addrlen
+                )
+            )
+            .WillOnce(ReturnArg<2>());
+    EXPECT_FUNCTION_CALL(recvSyscallMock, (6, _, sizeof(responseBuffer), _))
+            .WillOnce(DoAll(
+                    MemCpyArg<1>(responseBuffer, sizeof(responseBuffer)),
+                    ReturnArg<2>()
+            ));
     EXPECT_EQ(true, ifc->set_mac(ifc, "if0", &newAddr));
-
-    // Destory ifconfigurator
-    EXPECT_FUNCTION_CALL(closeSyscallMock, (6))
-        .WillOnce(Return(0));
-    ifc->destroy(ifc);
 }
-
